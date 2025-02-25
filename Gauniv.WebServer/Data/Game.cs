efcb6b9358aff317d488f480bd4d52672c1342f5
brieuc.mandin <brieuc.mandin@etu.estia.fr>
2025-02-25 15:40:45 +0100

    définistion jeu user et jeudto

--- Gauniv.WebServer/Data/Game.cs
+++ Gauniv.WebServer/Data/Game.cs
@@ -8,5 +8,32 @@ namespace Gauniv.WebServer.Data
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public byte[] Payload { get; set; } // Contenu binaire du jeu
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public class Category
+        {
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            [MaxLength(100)]
+            public string Name { get; set; }
+        }
     }
 }
